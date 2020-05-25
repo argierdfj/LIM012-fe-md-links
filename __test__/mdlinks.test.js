478919ccad9676cebf672caa4f808e69167cc3c7
argierdfj <argierincon@gmail.com>
2020-05-25 13:19:27 -0500

    terminando primeros tests

--- __test__/mdlinks.test.js
+++ __test__/mdlinks.test.js
@@ -1,10 +1,38 @@
 import {
   mdlinks,
+  relativeToAbsolute,
+  validateRoute,
+  validateFolder,
+  validateFile,
 } from '../src/index.js';
 
-describe('Función mdlinks', () => {
-  test('Probando función de mdlinks', () => {
+describe('Extraer links', () => {
+  test('Probando extraer links de archivos md', () => {
     let testing = mdlinks('./algo.md', null)
-    expect(testing[0]).toBe(['www.url.com', 'pagina', './archivo.md'])
+    expect(testing[0]).toBe(['www.url.com', 'pagina', './archivo.md']);
+  });
+});
+
+describe('Cambiar ruta relativa a absoluta', () => {
+  test('Probando convertir ruta relativa a absoluta', () => {
+    expect(relativeToAbsolute('./algo.md')).toBe('C:\\Users\\Estudiante\\Desktop\\Proyectos Laboratoria\\LIM012-fe-md-links\\algo.md');
+  });
+});
+
+describe('validar ruta', () => {
+  test('verificando si la ruta es válida', () => {
+    expect(validateRoute('./algo.md')).toBe(true);
+  });
+});
+
+describe('Verificar elemento', () => {
+  test('Verificando si es una carpeta', () => {
+    expect(validateFolder('./algo.md')).toBe(true);
+  });
+});
+
+describe('validar archivos md', () => {
+  test('verificando si el archivo es formato md', () => {
+    expect(validateFile('./algo.md')).toBe(false);
   });
 });
--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 export const mdlinks = (path, opts) => {
   return [[]];
 }
+
+export const relativeToAbsolute = (path) => {
+  return '';
+}
+
+export const validateRoute = (route) => {
+  return true;
+}
+
+export const validateFolder = (route) => {
+  return true;
+}
+
+export const validateFile = (route) => {
+  return false;
+}
