3b8aade238a76a4b6f25553fd63a18997a20c134
argierdfj <argierincon@gmail.com>
2020-05-29 18:55:07 -0500

    avance fn md links

--- src/index.js
+++ src/index.js
@@ -2,24 +2,18 @@ const axios = require('axios');
 const path = require('path');
 const fileSystem = require('fs');
 
-const mdlinks = async (elemPath, opts = undefined) => {
-  return axios.get(elemPath)
-  .then(({data}) => {
-    return data;
-  })
-  .catch()
-}
+// const mdlinks = async (elemPath, opts = undefined) => {
+//   return axios.get(elemPath)
+//   .then(({data}) => {
+//     return data;
+//   })
+//   .catch()
+// }
 
-
-
-//? ES RUTA ABSOLUTA?
 const relativeToAbsolute = (elemPath) => {
   return path.resolve(__dirname, elemPath)
 }
 
-
-
-//? ES RUTA VÁLIDA
 const validatePath = (elemPath) => {
   const directoryName = path.dirname(elemPath);
   const elemName = path.basename(elemPath);
@@ -36,58 +30,85 @@ const validatePath = (elemPath) => {
   return foundElem;
 }
 
-
-
-//? ¿ES UNA CARPETA?
-const validateElem = (elemPath, mdFiles = []) => {
-// para ver la extensión del elemento
+const validateElem = (elemPath, mdFilesPath = []) => {
   const extElem = path.extname(elemPath)
 
-  //* Si es una carpeta...
   if (extElem == '') {
     console.log('Es una carpeta');
     const elemList = fileSystem.readdirSync(elemPath);
 
-    //? CONTIENE ELEMENTOS?
-    //* Si contiene elementos
     if (elemList.length) {
       console.log('Contiene elementos');
-
       elemList.forEach((elem) => {
         const absPath = `${elemPath}\\${elem}`;
-        validateElem(absPath, mdFiles)
+        validateElem(absPath, mdFilesPath)
       });
     } else {
       console.log('No contiene elementos');
     }
-  
-
   } else {
     console.log('No es una carpeta');
     if (extElem == '.md') {
-      mdFiles.push(elemPath)
+      mdFilesPath.push(elemPath)
     }
   }
 
-  return mdFiles;
+  return mdFilesPath;
 }
 
-
-
-const searchingLinks = (mdFiles) => {
+const searchLinks = (mdFilesPath) => {
   const links = [];
-  mdFiles.forEach((mdFile) => {
-    const contentFile = fileSystem.readFileSync(mdFile, {encoding: 'utf8'})
+  mdFilesPath.forEach((mdFile) => {
+    const contentFile = fileSystem.readFileSync(mdFile, {
+      encoding: 'utf8'
+    })
     const regExpLinks = new RegExp('\\[.+\\]\\(.+\\)+', 'g')
-    links.push(contentFile.match(regExpLinks));
+    links.push({
+      path: mdFile,
+      links: contentFile.match(regExpLinks)
+    });
   });
-  return links;
+  return links.flat();
 }
+
+const mdlinks = (elemPath, opts = undefined) => {
+  const absPath = relativeToAbsolute(elemPath)
+
+  console.log('-'.repeat(absPath.length))
+  console.log(absPath);
+  console.log('-'.repeat(absPath.length))
+
+  console.log(validatePath(absPath))
+  const arrMdFilePath = validateElem(absPath)
+  
+  const linksFound = (searchLinks(arrMdFilePath))
+  console.log(linksFound);
+
+  // URL, TEXTO, RUTA
+  const elemMdLinks = [];
+  linksFound.forEach((linkFound) => {
+    linkFound.links.forEach((link) => {
+      const str = link.split('](');
+      elemMdLinks.push(
+        {
+          URL : str[1].slice(0, -1),
+          TEXTO: str[0].slice(2, -0),
+          RUTA: linkFound.path
+        }
+      ) 
+    });
+  });
+  
+  return elemMdLinks;
+}
+
 
 module.exports = {
   mdlinks,
   relativeToAbsolute,
   validatePath,
   validateElem,
-  searchingLinks
+  searchLinks
 }
+
+
--- src/prueba.js
+++ src/prueba.js
@@ -1,17 +1,5 @@
 const md = require('./index.js');
 const path = './'
-const absPath = md.relativeToAbsolute(path)
+const fnMdLinks = md.mdlinks(path);
 
-console.log('-'.repeat(absPath.length))
-console.log(absPath);
-console.log('-'.repeat(absPath.length))
-
-console.log(md.validatePath(absPath));
-// setTimeout(() => {
-//   console.log('-'.repeat(absPath.length));
-// }, 2000)
-const arrElems = md.validateElem(absPath)
-console.log(arrElems);
-
-console.log(md.searchingLinks(arrElems).flat())
-
+console.log(fnMdLinks);
