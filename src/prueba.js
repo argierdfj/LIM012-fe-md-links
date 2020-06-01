cb84814543cd43478896531cd79da80c76ce73d6
argierdfj <argierincon@gmail.com>
2020-06-01 09:56:29 -0500

    consumiendo la promesa de fn mdlinks

--- src/prueba.js
+++ src/prueba.js
@@ -2,4 +2,11 @@ const md = require('./index.js');
 const path = '../README.md'
 const fnMdLinks = md.mdlinks(path);
 
-console.log(fnMdLinks);
+fnMdLinks.then((links)=> {
+  links.forEach(link => {
+    console.log('links:' + link);
+  });
+})
+.catch((err)=>{
+  console.log('error:' + err);
+});
