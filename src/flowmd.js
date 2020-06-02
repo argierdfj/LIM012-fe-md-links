57ac8fbdc0cb0d122da0f5efe80fff87d84b42ce
argierdfj <argierincon@gmail.com>
2020-06-01 19:28:03 -0500

    separando fn del flujo a otro archivo

---
+++ src/flowmd.js
@@ -0,0 +1,62 @@
+const path = require('path');
+const fileSystem = require('fs');
+
+const flowmd = {
+  convertRelativeToAbsolutePath: (elemPath) => {
+    return path.resolve(__dirname, elemPath);
+  },
+  isValidPath: (elemPath) => {
+    const directoryName = path.dirname(elemPath);
+    const elemName = path.basename(elemPath);
+    const elemList = fileSystem.readdirSync(directoryName);
+
+    let foundElem = false;
+    if (elemList.includes(elemName)) {
+      foundElem = true
+    }
+    return foundElem;
+  },
+  getPathMdFile: (elemPath, arrMdFilesPath = []) => {
+    const extElem = path.extname(elemPath)
+    const elemName = path.basename(elemPath);
+
+    //? ES UN DIRECTORIO
+    if (extElem == '' && elemName.charAt(0) !== '.') {
+
+      const elemList = fileSystem.readdirSync(elemPath);
+
+      //? CONTIENE ELEMENTOS
+      if (elemList.length) {
+        elemList.forEach((elem) => {
+          const absPath = `${elemPath}\\${elem}`;
+          flowmd.getPathMdFile(absPath, arrMdFilesPath);
+        });
+      }
+    } else {
+      if (extElem == '.md') {
+        arrMdFilesPath.push(elemPath)
+      }
+    }
+
+    return arrMdFilesPath;
+  },
+  findLinks: (arrMdFilesPath) => {
+    const arrLinks = [];
+    if (arrMdFilesPath.length) {
+      arrMdFilesPath.forEach((mdFilePath) => {
+        const contentFile = fileSystem.readFileSync(mdFilePath, {
+          encoding: 'utf8'
+        })
+        const regExpLinks = new RegExp('\\[.+\\]\\(.+\\)+', 'g');
+        arrLinks.push({
+          path: mdFilePath,
+          links: contentFile.match(regExpLinks)
+        });
+      });
+    }
+
+    return arrLinks.flat();
+  }
+}
+
+module.exports = flowmd;
