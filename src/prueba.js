df2c493db7001ddf71255db6f98ee7ea615cd350
argierdfj <argierincon@gmail.com>
2020-06-01 19:30:10 -0500

    consumiendo promesa de fn mdlinks

--- src/prueba.js
+++ src/prueba.js
@@ -1,12 +1,25 @@
-const md = require('./index.js');
-const path = '../README.md'
-const fnMdLinks = md.mdlinks(path);
+const axios = require('axios');
+const mdLinks = require('./index.js');
+const path = './a.md'
+const fnMdLinks = mdLinks(path, { validate: true });
 
-fnMdLinks.then((links)=> {
-  links.forEach(link => {
-    console.log('links:' + link);
-  });
+fnMdLinks.then((links) => {
+  console.log(links)
 })
-.catch((err)=>{
-  console.log('error:' + err);
+.catch((err) => {
+  console.log(err.message);
 });
+
+
+// axios.get('https://us-central1-movistar-web-publica.cloudfunctions.net/ga_flow/hello_world/500')
+//   .then((data)=>{
+//     return {
+//       status: data.status,
+//       msg: 'OK'
+//     }
+//   }).catch(err =>{
+//     return {
+//       status: err.response.status,
+//       msg: 'FAIL'
+//     }
+//   })
