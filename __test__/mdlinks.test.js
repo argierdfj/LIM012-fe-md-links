d6fdd90850d3733877bc1e0053310fd81014aaac
argierdfj <argierincon@gmail.com>
2020-06-02 16:18:47 -0500

    tests de fn ruta absoluta y ruta válida

--- __test__/mdlinks.test.js
+++ __test__/mdlinks.test.js
@@ -1,14 +1,16 @@
-const md = require('../src/index.js');
+const mdlinks = require('../src/index.js');
+const flowmd = require('../src/flowmd.js');
 
-describe('Test asíncrono', () => {
-  test('Realizando una petición a una API', (done) => {
-    const api = 'https://us-central1-movistar-web-publica.cloudfunctions.net/ga_flow/hello_world'
-    md.mdlinks(api).then((data) => {     
-      expect(data.saludo).toBe('Hello World')
-      done();
-    })
-  });
-});
+
+// describe('Test asíncrono', () => {
+//   test('Realizando una petición a una API', (done) => {
+//     const api = 'https://us-central1-movistar-web-publica.cloudfunctions.net/ga_flow/hello_world'
+//     mdlinks(api).then((data) => {     
+//       expect(data.saludo).toBe('Hello World')
+//       done();
+//     })
+//   });
+// });
 
 // describe('Extraer links', () => {
 //   test('Probando extraer links de archivos md', () => {
@@ -17,17 +19,46 @@ describe('Test asíncrono', () => {
 //   });
 // });
 
-// describe('Cambiar ruta relativa a absoluta', () => {
-//   test('Probando convertir ruta relativa a absoluta', () => {
-//     expect(md.relativeToAbsolute('./algo.md')).toBe('C:\\Users\\Estudiante\\Desktop\\Proyectos Laboratoria\\LIM012-fe-md-links\\algo.md');
-//   });
-// });
+describe('RESOLVIENDO A RUTA ABSOLUTA', () => {
+  test('Probando con una ruta a un archivo .js', () => {
+    expect(flowmd.convertRelativeToAbsolutePath('../__test__/algo.js')).toBe(`${__dirname}\\algo.js`);
+  });
+  test('Probando con una ruta de una carpeta', () => {
+    expect(flowmd.convertRelativeToAbsolutePath('../__test__/prueba')).toBe(`${__dirname}\\prueba`);
+  });
+  // test('Probando con un String vacío', () => {
+  //   expect(flowmd.convertRelativeToAbsolutePath('')).toBe(__dirname);
+  //   });
+  test('Probando con un Número', () => {
+    expect(flowmd.convertRelativeToAbsolutePath(2)).toBe('');
+  });
+  test('Probando con un Array', () => {
+    expect(flowmd.convertRelativeToAbsolutePath([])).toBe('');
+  });
+  test('Probando con un Objeto', () => {
+    expect(flowmd.convertRelativeToAbsolutePath({})).toBe('');
+  });
+  test('Probando con un Booleano', () => {
+    expect(flowmd.convertRelativeToAbsolutePath(true)).toBe('');
+  });
+  test('Probando con una Función', () => {
+    expect(flowmd.convertRelativeToAbsolutePath(() => {})).toBe('');
+  });
+});
 
-// describe('validar ruta', () => {
-//   test('verificando si la ruta es válida', () => {
-//     expect(md.validateRoute('./algo.md')).toBe(true);
-//   });
-// });
+describe('VERIFICANDO SI LA RUTA ES VÁLIDA', () => {
+  test('Probando con la ruta a un elemento que si existe', () => {
+    const path = flowmd.convertRelativeToAbsolutePath('./a.md')
+    expect(flowmd.isValidPath(path)).toBe(true);
+  });
+  test('Probando con la ruta a un elemento que no existe', () => {
+    const path = flowmd.convertRelativeToAbsolutePath('../algo.md')
+    expect(flowmd.isValidPath(path)).toBe(false);
+  });
+  test('Probando con un número', () => {
+    expect(flowmd.isValidPath(6)).toBe(false);
+  });
+});
 
 // describe('Verificar elemento', () => {
 //   test('Verificando si es una carpeta', () => {
