8a82a9d833046296b25be76acfda67fa72879af7
argierdfj <argierincon@gmail.com>
2020-06-15 11:46:18 -0500

    añadiendo colores al output del CLI

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 const mdlinks = require('./index');
+const chalk = require('chalk');
 
 // Retorna un arr con los args pasados en la linea de comandos, el 1ero es el ejecutable node, el 2do la ruta del ejecutable y el 3ero los argumentos que el usuario le pase a mi fn.
 const [, , ...args] = process.argv;
@@ -26,37 +27,37 @@ const cli = (args) => {
     .then((links) => {
       if (stats) {
         //* Para contar la cantidad de total links
-        console.log('Total: ' + links.length);
+        console.log(`${chalk.magenta('Total:')} ${links.length}`);
 
         //* Para contar solo los links únicos.
         const newLinks = [];
         for (let i = 0; i < links.length; i++) {
           newLinks.push(JSON.stringify(links[i]));
         }
-        console.log('Unique: ' + [...new Set(newLinks)].length)
+        console.log(chalk.cyan('Unique:') + [...new Set(newLinks)].length);
 
         //* Para contar la cantidad de links rotos
         if (validate) {
           const brokenLinks = links.filter((link) => link.msg === 'FAIL');
-          console.log('Broken: ' + brokenLinks.length);
+          console.log(`${chalk.red('Broken:')} ${brokenLinks.length}`);
         }
       } else if (validate) {
         const relativePath = process.cwd();
         links.forEach((e) => {
-          const path = e.file.replace(relativePath, '.');
+          const path = chalk.cyan(e.file.replace(relativePath, '.'));
           const url = e.href;
-          const text = e.text;
-          const status = e.status;
-          const msg = e.msg;
+          const text = chalk.blue(e.text);
+          let status = (e.msg === 'OK') ? chalk.green(e.status) : chalk.red(e.status);
+          const msg = (e.msg === 'OK') ? chalk.green(e.msg) : chalk.red(e.msg);
 
           console.log(`${path} ${url} ${text} ${status} ${msg}`);
         });
       } else {
         const relativePath = process.cwd();
         links.forEach((e) => {
-          const path = e.file.replace(relativePath, '.');
+          const path = chalk.magenta(e.file.replace(relativePath, '.'));
           const url = e.href;
-          const text = e.text;
+          const text = chalk.blue(e.text);
 
           console.log(`${path} ${url} ${text}`);
         });
