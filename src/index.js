49db0496fdc80a830a6e25e8277f25b19fd52e09
argierdfj <argierincon@gmail.com>
2020-06-18 16:43:33 -0500

    refactorizando código

--- src/index.js
+++ src/index.js
@@ -1,71 +1,86 @@
-const method = require('./flowmd.js');
 const axios = require('axios');
+
+const {
+  convertRelativeToAbsolutePath,
+  isValidPath,
+  getPathMdFile,
+  findLinks
+} = require('./flowmd.js');
+
+//? Fn para el validate 
+const validateWithAxios = (arrMdFileLinks) => {
+  const arrLinkStatus = [];
+  arrMdFileLinks.forEach((mdFileLinks) => {
+    arrLinkStatus.push(axios.get(mdFileLinks.href)
+      .then((data) => {
+        return {
+          ...mdFileLinks,
+          status: data.status,
+          msg: 'OK'
+        }
+      })
+      .catch((err) => {
+        return {
+          ...mdFileLinks,
+          status: err.response ? err.response.status : 999,
+          msg: 'FAIL'
+        }
+      })
+    );
+  });
+
+  return arrLinkStatus;
+}
+
+//? Fn para el arr de objetos de mdlinks 
+const objMdlinks = (arrLinksFound) => {
+  const arrMdFileLinks = [];
+
+  if (arrLinksFound.length) {
+    arrLinksFound.forEach((linkFound) => {
+      linkFound.links.forEach((link) => {
+        const str = link.split('](');
+        arrMdFileLinks.push({
+          href: str[1].trim().slice(0, -1),
+          text: str[0].trim().slice(1, 50),
+          file: linkFound.path
+        })
+      });
+    });
+  }  
+  return arrMdFileLinks;
+};
 
 const mdlinks = (elemPath, options = {
     validate: false
   }) =>
   new Promise((resolve, reject) => {
-    const absPath = method.convertRelativeToAbsolutePath(elemPath);
+    const absPath = convertRelativeToAbsolutePath(elemPath);
 
-    if (method.isValidPath(absPath)) {
-      const arrMdFilePath = method.getPathMdFile(absPath);
-
-      if (arrMdFilePath.length) {
-        const arrLinksFound = method.findLinks(arrMdFilePath);
-
-        const arrMdFileLinks = [];
-
-        if (arrLinksFound.length) {
-          arrLinksFound.forEach((linkFound) => {
-            if (linkFound.links) {
-              linkFound.links.forEach((link) => {
-                const str = link.split('](');
-                arrMdFileLinks.push({
-                  href: str[1].trim().slice(0, -1),
-                  text: str[0].trim().slice(1, 50),
-                  file: linkFound.path
-                })
-              });
-            }
-          });
-        }
-        
-        if (arrMdFileLinks.length) {
-          if (options.validate) {
-            const arrLinkStatus = [];
-            arrMdFileLinks.forEach((mdFileLinks) => {
-              arrLinkStatus.push(axios.get(mdFileLinks.href)
-                .then((data) => {
-                  return {
-                    ...mdFileLinks,
-                    status: data.status,
-                    msg: 'OK'
-                  }
-                })
-                .catch((err) => {
-                  return {
-                    ...mdFileLinks,
-                    status: err.response ? err.response.status : 999,
-                    msg: 'FAIL'
-                  }
-                })
-              );
-            });
-            resolve(Promise.all(arrLinkStatus))
-          } else {
-            resolve(arrMdFileLinks);
-          }
-        } else {
-          reject(new Error('No se encontraron links'));
-        }
-
-      } else {
-        reject(new Error('No se encontraron archivos .md'));
-      }
-
-    } else {
+    if (!isValidPath(absPath)) {
       reject(new Error('La ruta ingresada no es válida'));
     }
+    
+    const arrMdFilePath = getPathMdFile(absPath);
+
+    if (!arrMdFilePath.length) {
+      reject(new Error('No se encontraron archivos .md'));
+    }
+
+    const arrLinksFound = findLinks(arrMdFilePath);
+    const arrMdFileLinks = objMdlinks(arrLinksFound);
+
+    if (!arrMdFileLinks.length) {
+      reject(new Error('No se encontraron links'));
+    } 
+
+    if (options.validate) {
+      const arrLinkStatus = validateWithAxios(arrMdFileLinks);
+      resolve(Promise.all(arrLinkStatus));
+    } else {
+      resolve(arrMdFileLinks);
+    }
+
   });
 
 module.exports = mdlinks;
