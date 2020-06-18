5621b2260e4272783df2f5c4ebc08173cd541f90
argierdfj <argierincon@gmail.com>
2020-06-18 15:57:50 -0500

    Corrigiendo las rutas de los tests para que sirvan en otros OS close #8

--- __test__/mdlinks.test.js
+++ __test__/mdlinks.test.js
@@ -1,98 +1,124 @@
+const {
+  convertRelativeToAbsolutePath,
+  isValidPath,
+  getPathMdFile,
+  findLinks
+} = require('../src/flowmd.js');
+
 const mockAxios = require('axios');
 const mdlinks = require('../src/index.js');
-const flowmd = require('../src/flowmd.js');
+const path = require('path');
 
 describe('RESOLVIENDO A RUTA ABSOLUTA', () => {
   test('Probando con una ruta a un archivo .html', () => {
-    expect(flowmd.convertRelativeToAbsolutePath('../__test__/test.html')).toBe(`${__dirname}\\test.html`);
+    expect(convertRelativeToAbsolutePath('./__test__/test.html')).toBe(path.resolve(`${__dirname}\\test.html`));
   });
   test('Probando con una ruta de una carpeta', () => {
-    expect(flowmd.convertRelativeToAbsolutePath('../__test__/test')).toBe(`${__dirname}\\test`);
+    expect(convertRelativeToAbsolutePath('./__test__/test')).toBe(path.resolve(`${__dirname}\\test`));
   });
   test('Probando con un String vacío', () => {
-    expect(flowmd.convertRelativeToAbsolutePath('')).toBe('C:\\Users\\Estudiante\\Desktop\\Proyectos Laboratoria\\LIM012-fe-md-links\\src');
+    expect(convertRelativeToAbsolutePath('')).toBe(path.resolve(process.cwd()));
   });
   test('Probando con un Número', () => {
-    expect(flowmd.convertRelativeToAbsolutePath(2)).toBe('');
+    expect(convertRelativeToAbsolutePath(2)).toBe('');
   });
   test('Probando con un Array', () => {
-    expect(flowmd.convertRelativeToAbsolutePath([])).toBe('');
+    expect(convertRelativeToAbsolutePath([])).toBe('');
   });
   test('Probando con un Objeto', () => {
-    expect(flowmd.convertRelativeToAbsolutePath({})).toBe('');
+    expect(convertRelativeToAbsolutePath({})).toBe('');
   });
   test('Probando con un Booleano', () => {
-    expect(flowmd.convertRelativeToAbsolutePath(true)).toBe('');
+    expect(convertRelativeToAbsolutePath(true)).toBe('');
   });
   test('Probando con una Función', () => {
-    expect(flowmd.convertRelativeToAbsolutePath(() => {})).toBe('');
+    expect(convertRelativeToAbsolutePath(() => {})).toBe('');
   });
 });
 
 describe('VERIFICANDO SI LA RUTA ES VÁLIDA', () => {
   test('Probando con la ruta a un elemento que si existe', () => {
-    const absPath = flowmd.convertRelativeToAbsolutePath('../paso-a-paso.md')
-    expect(flowmd.isValidPath(absPath)).toBe(true);
+    const absPath = convertRelativeToAbsolutePath('./paso-a-paso.md')
+    expect(isValidPath(absPath)).toBe(true);
   });
   test('Probando con la ruta a un elemento que no existe', () => {
-    const absPath = flowmd.convertRelativeToAbsolutePath('../test.md');
-    expect(flowmd.isValidPath(absPath)).toBe(false);
+    const absPath = convertRelativeToAbsolutePath('./test.md');
+    expect(isValidPath(absPath)).toBe(false);
   });
   test('Probando con un String vacío', () => {
-    expect(flowmd.isValidPath('')).toBe(false);
+    expect(isValidPath('')).toBe(false);
   });
   test('Probando con un Número', () => {
-    expect(flowmd.isValidPath(2)).toBe(false);
+    expect(isValidPath(2)).toBe(false);
   });
   test('Probando con un Array', () => {
-    expect(flowmd.isValidPath([])).toBe(false);
+    expect(isValidPath([])).toBe(false);
   });
   test('Probando con un Objeto', () => {
-    expect(flowmd.isValidPath({})).toBe(false);
+    expect(isValidPath({})).toBe(false);
   });
   test('Probando con un Booleano', () => {
-    expect(flowmd.isValidPath(true)).toBe(false);
+    expect(isValidPath(true)).toBe(false);
   });
   test('Probando con una Función', () => {
-    expect(flowmd.isValidPath(() => {})).toBe(false);
+    expect(isValidPath(() => {})).toBe(false);
   });
 });
 
 describe('OBTENIENDO RUTAS DE ARCHIVOS MD', () => {
   test('Probando con un directorio vacío', () => {
-    const absPath = flowmd.convertRelativeToAbsolutePath('../__test__/test_2');
-    expect(flowmd.getPathMdFile(absPath)).toEqual([]);
+    const absPath = convertRelativeToAbsolutePath('./__test__/test_2');
+    expect(getPathMdFile(absPath)).toEqual([]);
   });
   test('Probando con un directorio con elementos', () => {
-    const absPath = flowmd.convertRelativeToAbsolutePath('../__test__');
-    expect(flowmd.getPathMdFile(absPath)).toEqual([
-      `C:\\Users\\Estudiante\\Desktop\\Proyectos Laboratoria\\LIM012-fe-md-links\\__test__\\test\\test.md`,
-    ]);
+    const absPath = convertRelativeToAbsolutePath('./__test__');
+    expect(getPathMdFile(absPath)).toEqual([path.resolve('__test__/test/test.md')]);
   });
   test('Probando con un archivo md', () => {
-    const absPath = flowmd.convertRelativeToAbsolutePath('../paso-a-paso.md');
-    expect(flowmd.getPathMdFile(absPath)).toEqual(['C:\\Users\\Estudiante\\Desktop\\Proyectos Laboratoria\\LIM012-fe-md-links\\paso-a-paso.md']);
+    const absPath = convertRelativeToAbsolutePath('./paso-a-paso.md');
+    expect(getPathMdFile(absPath)).toEqual([path.resolve('./paso-a-paso.md')]);
+  });
+  test('Probando con un String vacío', () => {
+    expect(getPathMdFile('')).toBe('');
+  });
+  test('Probando con un Número', () => {
+    expect(getPathMdFile(2)).toBe('');
+  });
+  test('Probando con un Array', () => {
+    expect(getPathMdFile([])).toBe('');
+  });
+  test('Probando con un Objeto', () => {
+    expect(getPathMdFile({})).toBe('');
+  });
+  test('Probando con un Booleano', () => {
+    expect(getPathMdFile(true)).toBe('');
+  });
+  test('Probando con una Función', () => {
+    expect(getPathMdFile(() => {})).toBe('');
   });
 });
 
 describe('ENCONTRANDO LINKS EN ARCHIVOS MD', () => {
   test('Probando con un archivo md que contiene enlaces', () => {
-    expect(flowmd.findLinks(['C:\\Users\\Estudiante\\Desktop\\Proyectos Laboratoria\\LIM012-fe-md-links\\__test__\\test\\test.md'])[0].links[0]).toBe('[learnyounode](https://github.com/workshopper/learnyounode)');
+    expect(findLinks([path.resolve('__test__/test/test.md')])[0].links[0]).toBe('[learnyounode](https://github.com/workshopper/learnyounode)');
   });
   test('Probando con un string', () => {
-    expect(flowmd.findLinks('array de links')).toEqual([]);
+    expect(findLinks('array de links')).toEqual([]);
   });
   test('Probando con un array vacío', () => {
-    expect(flowmd.findLinks([])).toEqual([]);
+    expect(findLinks([])).toEqual([]);
+  });
+  test('Probando con un archivo md que no contiene enlaces', () => {
+    expect(findLinks([path.resolve('paso-a-paso.md')])).toEqual([]);
   });
 });
 
 describe('PROBANDO FUNCIÓN MD LINKS', () => {
   test('Probando con un archivo md que contiene enlaces', (done) => {
-    const api = flowmd.convertRelativeToAbsolutePath('../__test__/test/test.md');
+    const api = convertRelativeToAbsolutePath('./__test__/test/test.md');
     mdlinks(api).then((links) => {
       expect(links[0]).toEqual({
-        'file': 'C:\\Users\\Estudiante\\Desktop\\Proyectos Laboratoria\\LIM012-fe-md-links\\__test__\\test\\test.md',
+        'file': api,
         'href': 'https://github.com/workshopper/learnyounode',
         'text': 'learnyounode'
       })
@@ -100,10 +126,10 @@ describe('PROBANDO FUNCIÓN MD LINKS', () => {
     })
   });
   test('Probando con un objeto vacío', (done) => {
-    const api = flowmd.convertRelativeToAbsolutePath('../__test__/test/test.md');
+    const api = convertRelativeToAbsolutePath('./__test__/test/test.md');
     mdlinks(api, {}).then((links) => {
       expect(links[0]).toEqual({
-        'file': 'C:\\Users\\Estudiante\\Desktop\\Proyectos Laboratoria\\LIM012-fe-md-links\\__test__\\test\\test.md',
+        'file': api,
         'href': 'https://github.com/workshopper/learnyounode',
         'text': 'learnyounode'
       })
@@ -111,10 +137,10 @@ describe('PROBANDO FUNCIÓN MD LINKS', () => {
     })
   });
   test('Probando con un directorio que contiene elementos', (done) => {
-    const api = flowmd.convertRelativeToAbsolutePath('../__test__/');
+    const api = convertRelativeToAbsolutePath('./__test__/');
     mdlinks(api).then((links) => {
       expect(links[0]).toEqual({
-        'file': 'C:\\Users\\Estudiante\\Desktop\\Proyectos Laboratoria\\LIM012-fe-md-links\\__test__\\test\\test.md',
+        'file': path.resolve('__test__/test/test.md'),
         'href': 'https://github.com/workshopper/learnyounode',
         'text': 'learnyounode'
       })
@@ -122,21 +148,21 @@ describe('PROBANDO FUNCIÓN MD LINKS', () => {
     })
   });
   test('Probando con un archivo html', (done) => {
-    const api = flowmd.convertRelativeToAbsolutePath('../__test__/test/test.html');
+    const api = convertRelativeToAbsolutePath('./__test__/test/test.html');
     mdlinks(api).catch((err) => {
       expect(err).toEqual(new Error('No se encontraron archivos .md'));
       done();
     })
   });
   test('Probando con una ruta que no es válida', (done) => {
-    const api = flowmd.convertRelativeToAbsolutePath('.test.html');
+    const api = convertRelativeToAbsolutePath('./test.html');
     mdlinks(api).catch((err) => {
       expect(err).toEqual(new Error('La ruta ingresada no es válida'));
       done();
     })
   });
   test('Probando con un archivo md que no continen links', (done) => {
-    const api = flowmd.convertRelativeToAbsolutePath('../paso-a-paso.md');
+    const api = convertRelativeToAbsolutePath('./paso-a-paso.md');
     mdlinks(api).catch((err) => {
       expect(err).toEqual(new Error('No se encontraron links'));
       done();
@@ -152,13 +178,13 @@ describe('PROBANDO FUNCIÓN MD LINKS', () => {
     );
 
 
-    const api = flowmd.convertRelativeToAbsolutePath('../__test__/test/test.md');
+    const api = convertRelativeToAbsolutePath('./__test__/test/test.md');
     mdlinks(api, {
         validate: true
       })
       .then((links) => {
         expect(links[2]).toEqual({
-          'file': 'C:\\Users\\Estudiante\\Desktop\\Proyectos Laboratoria\\LIM012-fe-md-links\\__test__\\test\\test.md',
+          'file': api,
           'href': "https://github.com/stevekane/promise-it-wont-hurt",
           'msg': "OK",
           'status': 200,
@@ -178,7 +204,7 @@ describe('PROBANDO FUNCIÓN MD LINKS', () => {
     );
 
 
-    const api = flowmd.convertRelativeToAbsolutePath('../__test__/test/test.md');
+    const api = convertRelativeToAbsolutePath('./__test__/test/test.md');
     mdlinks(api, {
         validate: true
       })
@@ -186,7 +212,7 @@ describe('PROBANDO FUNCIÓN MD LINKS', () => {
         expect(links[3]).toEqual({
           href: 'https://us-central1-movistar-web-publica.cloudfunctions.net/ga_flow/hello_world/400',
           text: 'error 400 test',
-          file: 'C:\\Users\\Estudiante\\Desktop\\Proyectos Laboratoria\\LIM012-fe-md-links\\__test__\\test\\test.md',
+          file: api,
           status: 400,
           msg: 'FAIL'
         })
@@ -204,7 +230,7 @@ describe('PROBANDO FUNCIÓN MD LINKS', () => {
     );
 
 
-    const api = flowmd.convertRelativeToAbsolutePath('../__test__/test/test.md');
+    const api = convertRelativeToAbsolutePath('./__test__/test/test.md');
     mdlinks(api, {
         validate: true
       })
@@ -212,7 +238,7 @@ describe('PROBANDO FUNCIÓN MD LINKS', () => {
         expect(links[4]).toEqual({
           href: 'https://us-central1-movistar-web-publica.cloudfunctions.net/ga_flow/hello_world/500',
           text: 'error 500 test',
-          file: 'C:\\Users\\Estudiante\\Desktop\\Proyectos Laboratoria\\LIM012-fe-md-links\\__test__\\test\\test.md',
+          file: api,
           status: 500,
           msg: 'FAIL'
         })
@@ -226,7 +252,7 @@ describe('PROBANDO FUNCIÓN MD LINKS', () => {
     );
 
 
-    const api = flowmd.convertRelativeToAbsolutePath('../__test__/test/test.md');
+    const api = convertRelativeToAbsolutePath('./__test__/test/test.md');
     mdlinks(api, {
         validate: true
       })
@@ -234,7 +260,7 @@ describe('PROBANDO FUNCIÓN MD LINKS', () => {
         expect(links[4]).toEqual({
           href: 'https://us-central1-movistar-web-publica.cloudfunctions.net/ga_flow/hello_world/500',
           text: 'error 500 test',
-          file: 'C:\\Users\\Estudiante\\Desktop\\Proyectos Laboratoria\\LIM012-fe-md-links\\__test__\\test\\test.md',
+          file: api,
           status: 999,
           msg: 'FAIL'
         })
